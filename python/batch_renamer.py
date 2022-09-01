72688b0dee0c88e0caf7c7f2aad14cb347726268
alejandro galaz <alejandrogalaz21@gmail.com>
2022-08-31 20:41:46 -0500

    add the extension of the file

--- python/batch_renamer.py
+++ python/batch_renamer.py
@@ -6,7 +6,7 @@ replace = "file"
 type_filter = ".py"
 
 # get all files from current directory
-dir_content = os.listdir('E:\Pictures')
+dir_content = os.listdir('.')
 docs = [doc for doc in dir_content if os.path.isfile(doc)]
 renamed = 0
 
@@ -14,17 +14,16 @@ print(f"{len(docs)} of {len(dir_content)} elements are files.")
 
 # go through all the files and check if they match the search pattern
 for doc in docs:
-    # # separate name from file extension
-    # doc_name, filetype = os.path.splitext(doc)
-
-    # # filter for files with the right extension
-    # if filetype == type_filter:
+    # separate name from file extension
+    doc_name, filetype = os.path.splitext(doc)
+    print(f"doc_name {doc_name}, filetype {filetype} ")
+    # filter for files with the right extension
+    if filetype == type_filter:
         # check if search text is in doc name
-        if search in doc:
-            
-            #replace with the given text
-            new_name = doc.replace(search, replace) 
-            # os.rename(doc, new_name)
+        if search in doc_name:  
+            # replace with the given text
+            new_name = doc_name.replace(search, replace) + filetype
+            os.rename(doc, new_name)
             renamed += 1
 
             print(f"Renamed file {doc} to {new_name}")
